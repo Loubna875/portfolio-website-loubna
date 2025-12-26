d1e5e636be5a90b603e63563dc2d7a167100447b
Loubna875 <loubna_othmani@yahoo.com>
2025-12-26 15:44:51 +0100

    Add full Grade 3 contact form and correct page structure

---
+++ contact.js
@@ -0,0 +1,11 @@
+// Validation functions will be added here
+
+function validateName () {}
+function validateEmail () {}
+function validateMessage () {}
+
+function showError () {}
+function clearError () {}
+
+function clearForm () {}
+
