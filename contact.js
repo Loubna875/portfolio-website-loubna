2e047f342e49a8cd4d1ff06cdd83a6386c0d63e6
Loubna875 <loubna_othmani@yahoo.com>
2025-12-27 12:53:48 +0100

    Implement first name validation function

--- contact.js
+++ contact.js
@@ -1,6 +1,28 @@
+// DOM Selection
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+const charCounter = document.querySelector(".char-counter");
+const successMessage = document.getElementById("successMessage");
+const form = document.getElementById("contactForm");
+
+
 // Validation functions will be added here
 
-function validateName () {}
+function validateFirstName () {
+ const value = firstName.value.trim();
+ const regex = /^[A-Za-z]+$/;
+ if (!regex.test(value)) {
+    showError(firstName, "First name must cintain only letters");
+    return false;
+    }
+    clearError(firstName);
+    return true;
+    }
+
 function validateEmail () {}
 function validateMessage () {}
 
